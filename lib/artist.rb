90c8c312c7a59979d31fe8bca302bc6fac98e3d8
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-07 18:37:13 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def new_song(name, genre) 
+    new_song = Song.new(name, genre)
+    self << new_song 
+  end 
+  
+  def songs 
+    songs.all.select do |song|
+      self.song 
+    end 
+  end 
+  
+  def genre 
+    
+  end 
+end 
