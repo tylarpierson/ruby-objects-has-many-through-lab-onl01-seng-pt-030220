926d5b2dff55b5737af0d8d5a8b42d1a2b62e9ad
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-07 18:42:46 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,4 +11,12 @@ class Genre
   def self.all
     @@all
   end 
+  
+  def songs 
+    Song.all.select {|song| song.genre == self} 
+  end 
+  
+  def artists 
+    Song.all.select {|song| song.artist == artist} 
+  end 
 end 
